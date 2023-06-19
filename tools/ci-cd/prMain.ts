083d23a766b8064388573d08737ae3bee3516be7
Quyet Tran <tranquyet.dev@gmail.com>
2023-06-19 13:45:28 +0700

    ci(github): assume role and deploy cloudfront matrix

--- tools/ci-cd/prMain.ts
+++ tools/ci-cd/prMain.ts
@@ -1,63 +1,23 @@
-import * as core from '@actions/core';
-import isCI from 'is-ci';
-import { getAffectedProjects } from './nx';
-import { IDeployMatrix, getDeployConfig } from './deploy';
-
-function setOutput(name: string, value: any) {
-  const output = JSON.stringify(value || { run: false, name: 'SKIP' });
-  if (isCI) {
-    core.setOutput(`${name}`, `${output}`);
-  } else {
-    console.log(`${name}=${output}`);
-  }
-}
+import { getAffectedProjects, getEnvironment, setOutput } from './utils';
+import { getCloudFrontMatrix, getECSMatrix } from './matrix';
 
 (function setMatrix() {
-  console.log('setMatrix...');
-
   const affectedProjects = getAffectedProjects();
+  const environment = getEnvironment(process.env.CURRENT_BRANCH || '');
+
+  if (!environment) {
+    throw new Error(
+      `Environment is not defined for branch ${process.env.CURRENT_BRANCH}`
+    );
+  }
+
   console.log(`affectedProjects`, affectedProjects);
-
-  const allMatrix: IDeployMatrix = {
-    ecsMatrix: {
-      include: [],
-    },
-    cloudfrontMatrix: {
-      include: [],
-    },
-  };
-
-  affectedProjects.forEach((project) => {
-    const { matrix, projectId } = getDeployConfig(project);
-
-    if (matrix.ecsMatrix) {
-      allMatrix.ecsMatrix.include.push({
-        run: true,
-        name: projectId,
-      });
-    }
-
-    if (matrix.cloudfrontMatrix) {
-      allMatrix.cloudfrontMatrix.include.push({
-        run: true,
-        name: projectId,
-      });
-    }
-  });
-
-  const skipped = { run: false, name: 'SKIP' };
-  if (allMatrix.ecsMatrix.include.length === 0) {
-    allMatrix.ecsMatrix.include.push(skipped);
-  }
-
-  if (allMatrix.cloudfrontMatrix.include.length === 0) {
-    allMatrix.cloudfrontMatrix.include.push(skipped);
-  }
-
-  console.log(`ecsMatrix`, allMatrix.ecsMatrix);
-  console.log(`cloudfrontMatrix`, allMatrix.cloudfrontMatrix);
+  console.log(`environment`, environment);
 
   setOutput('affectedProjects', affectedProjects);
-  setOutput('ecsMatrix', allMatrix.ecsMatrix);
-  setOutput('cloudfrontMatrix', allMatrix.cloudfrontMatrix);
+  setOutput('ecsMatrix', getECSMatrix(environment, affectedProjects));
+  setOutput(
+    'cloudfrontMatrix',
+    getCloudFrontMatrix(environment, affectedProjects)
+  );
 })();
