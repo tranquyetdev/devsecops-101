f0b0d9d99614df02d969768a0495033996def7a6
Quyet Tran <tranquyet.dev@gmail.com>
2023-06-19 15:47:57 +0700

    ci(infra): remove unused infra

--- infra/s3-static-website-cloudfront/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cloudfront_domain_name" {
-  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
-}
-
-output "cloudfront_custom_domain_name" {
-  value = "https://${local.subdomain}.${var.zone_name}"
-}
